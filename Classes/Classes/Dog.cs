42d600747185716772f295f848b6278f42c879bc
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-12-09 21:04:38 +0200

    code written in the class for better learning

---
+++ Classes/Classes/Dog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Dog
+    {
+        //fields
+        private string _name;
+        string _owner; // private by default
+        private readonly DateTime _birthDate;
+        public const decimal PI = 3.141516M;
+        public static int nrOfInstances;
+
+        //parameterless constructor - in constructor ar trebui sa punem parametrii obligatorii care fac ca obiectul sa fie sanatos
+        public Dog()
+        {
+
+        }
+
+        public Dog(string name)
+        {
+            _name = name;
+        }
+
+        public Dog(string name, string owner)
+        {
+            _name = name;
+            _owner = owner;
+            _birthDate = DateTime.Now;
+            nrOfInstances++;
+        }
+
+        public void Bark()
+        {
+            string _name = "local name";
+            Console.WriteLine("Ham Ham");
+            decimal awesomePI = PI * 2;
+        }
+
+        public static int GetTotalInstancesCreated()
+        {
+            return nrOfInstances;
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dog dog = new Dog();
+
+            Dog myDog = new Dog("Grivei", "Maricica");
+            Dog yourDog = new Dog("Lala", "Ion");
+
+            //myDog._name = "Azorel";
+            //myDog._birthDate = DateTime.Now.AddDays(100);
+            decimal piDog = Dog.PI;
+
+            Console.WriteLine(Dog.nrOfInstances);
+            Console.WriteLine(Dog.GetTotalInstancesCreated());
+        }
+    }
+}
